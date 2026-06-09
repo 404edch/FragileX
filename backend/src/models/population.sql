5c03c82b2e92b78dffc9bedc3eca16aef0e95cd8
Lunna <lunnadamo@gmail.com>
2026-06-09 16:10:48 -0300

    Sintomas

---
+++ backend/src/models/population.sql
@@ -0,0 +1,15 @@
+
+INSERT INTO checklist (descricao_sintoma, peso_masculino, peso_feminino) VALUES
+('Atraso na fala', 0.14, 0.01),
+('Dificuldades de aprendizado', 0.18, 0.28),
+('Déficit de atenção', 0.17, 0.12),
+('Deficiência intelectual (ID)', 0.32, 0.20),
+('Hiperatividade', 0.12, 0.04),
+('Agressividade', 0.01, 0.02),
+('Evita contato visual', 0.06, 0.08),
+('Evita contato físico', 0.04, 0.07),
+('Movimentos intencionais, repetitivos e rítmicos', 0.17, 0.05),
+('Hiperflexibilidade articular (hipermobilidade)', 0.19, 0.04),
+('Macroorquidia', 0.26, 0.00),
+('Face alongada, mandíbula proeminente e/ou orelhas de abano', 0.29, 0.09);
+
